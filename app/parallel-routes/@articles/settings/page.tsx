fb91eee6d1b30f9f7b44305e1ac4a2194f4356d0
ssabii <fhy1930@gmail.com>
2024-08-18 02:10:08 +0900

    feat: 병렬 라우팅 하위 경로 추가

---
+++ app/parallel-routes/@articles/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function ArticlesSettingsPage() {
+  return (
+    <h2 className='rounded-lg bg-sky-300 p-4 text-2xl text-gray-900 shadow-md'>
+      Articles Settings
+    </h2>
+  );
+}
--- app/parallel-routes/@login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <h2 className='rounded-lg bg-purple-300 p-4 text-2xl text-gray-900 shadow-md'>
-      Login
-    </h2>
-  );
-}
