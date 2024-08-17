414864df2ce3680ced15f457b8159e3823da768a
ssabii <fhy1930@gmail.com>
2024-08-18 02:01:23 +0900

    feat: 병렬 라우팅 조건부 렌더링

---
+++ app/parallel-routes/@login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <h2 className='rounded-lg bg-purple-300 p-4 text-2xl text-gray-900 shadow-md'>
+      Login
+    </h2>
+  );
+}
--- app/parallel-routes/layout.tsx
+++ app/parallel-routes/layout.tsx
@@ -1,14 +1,20 @@
-import { Suspense } from "react";
-
 export default function ParallelRoutesLayout({
   children,
   articles,
   users,
+  login,
 }: {
   children: React.ReactNode;
   articles: React.ReactNode
   users: React.ReactNode;
+  login: React.ReactNode;
 }) {
+  const isLoggedIn = false
+
+  if (!isLoggedIn) {
+    return login;
+  }
+
   return (
     <section className="p-8">
       <h1 className="mb-4 text-3xl font-bold text-neutral-900">Parallel Routes Example</h1>
