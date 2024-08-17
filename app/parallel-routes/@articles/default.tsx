ff0a90607875d6af9ae9bb1af092750ed291d03e
ssabii <fhy1930@gmail.com>
2024-08-18 02:44:03 +0900

    feat: default page 예제 구현

--- app/parallel-routes/@articles/default.tsx
+++ app/parallel-routes/@articles/default.tsx
@@ -1,7 +1,7 @@
-export default async function ArticlesSettingsPage() {
+export default async function ArticlesDefaultPage() {
   return (
     <h2 className='rounded-lg bg-sky-300 p-4 text-2xl text-gray-900 shadow-md'>
-      Articles Settings
+      Articles Default
     </h2>
   );
 }
