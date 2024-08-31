4255446d89e7bfdb7aae4a144758a51e15d6c0dc
ssabii <fhy1930@gmail.com>
2024-08-31 22:24:01 +0900

    feat: MessageContext Props 수정

--- app/sequential-rendering/MessageContext.tsx
+++ app/sequential-rendering/MessageContext.tsx
@@ -1,17 +1,17 @@
 import { createContext, useContext, useState, ReactNode, SetStateAction, Dispatch } from 'react';
 
 interface MessageContextProps {
-  index: number;
-  setIndex: Dispatch<SetStateAction<number>>;
+  currentIndex: number;
+  setCurrentIndex: Dispatch<SetStateAction<number>>;
 }
 
 const MessageContext = createContext<MessageContextProps | undefined>(undefined);
 
 export const MessageProvider = ({ children }: { children: ReactNode }) => {
-  const [index, setIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <MessageContext.Provider value={{ index, setIndex }}>
+    <MessageContext.Provider value={{ currentIndex, setCurrentIndex }}>
       {children}
     </MessageContext.Provider>
   );
@@ -20,7 +20,7 @@ export const MessageProvider = ({ children }: { children: ReactNode }) => {
 export const useMessage = (): MessageContextProps => {
   const context = useContext(MessageContext);
   if (!context) {
-    throw new Error('useMessage must be used within a MessagProvider');
+    throw new Error('useMessage must be used within a MessageProvider');
   }
   return context;
 };
