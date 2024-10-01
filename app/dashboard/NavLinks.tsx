1742c81f0b95f4e3b0e2bb9bf4dabb5b8aa969ae
ssabii <fhy1930@gmail.com>
2024-10-01 23:58:20 +0900

    feat: pathname을 사용해서 NavLinks 스타일 수정

--- app/dashboard/NavLinks.tsx
+++ app/dashboard/NavLinks.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import RightArrow from "@/components/icons/RightArrow";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <header className="h-16 border border-b-gray-300 p-4">
       <ol className="flex items-center gap-1 fill-gray-500 text-lg font-bold text-gray-500">
         <li>
           <Link
             href="/dashboard"
-            className='hover:text-gray-700'
+            className={clsx({
+              'hover:text-gray-700': pathname !== '/dashboard',
+              'text-blue-700': pathname === '/dashboard',
+            })}
           >
             Dashboard
           </Link>
@@ -19,7 +28,10 @@ function NavLinks() {
         <li>
           <Link
             href="/dashboard/settings"
-            className='hover:text-gray-700'
+            className={clsx({
+              'hover:text-gray-700': pathname !== '/dashboard/settings',
+              'text-blue-700': pathname === '/dashboard/settings',
+            })}
           >
             Settings
           </Link>
