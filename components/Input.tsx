8244889d7430952dca25b9d114ad86aab381966c
ssabii <fhy1930@gmail.com>
2024-10-14 00:26:59 +0900

    feat: Input 컴포넌트 props 받게 수정

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,15 @@
-function Input() {
+import { InputHTMLAttributes } from "react";
+
+function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <input className={`
-      block rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900
-      focus:border-blue-500 focus:ring-blue-500
-      dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400
-      dark:focus:border-blue-500 dark:focus:ring-blue-500
-    `}
+    <input
+      className={`
+        block rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900
+        focus:border-blue-500 focus:ring-blue-500
+        dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400
+        dark:focus:border-blue-500 dark:focus:ring-blue-500
+      `}
+      {...props}
     />
   );
 }
