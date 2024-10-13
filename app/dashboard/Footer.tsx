5d6ae0a1492021fb42b05d72acc9d5f11f9c545c
ssabii <fhy1930@gmail.com>
2024-10-14 00:26:00 +0900

    chore: 불필요 파일 제거

--- app/dashboard/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="flex h-16 items-center border border-t-gray-300 p-4">
-      <p className="text-lg">Footer</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- app/dashboard/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <h1 className="text-2xl font-bold">
-      Hello, Settings Page!
-    </h1>
-  );
-}
