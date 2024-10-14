b7ce55efdda791f715e9a6a1e554d29006fcdbbe
ssabii <fhy1930@gmail.com>
2024-10-14 23:23:12 +0900

    feat: generateStaticParams 예제 구현

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+  const posts = [1, 2, 3]
+
+  return posts.map((post) => ({
+    slug: post,
+  }))
+}
+
 export default function Page({ params }: { params: { slug: string } }) {
   return (
     <div>My Post: {params.slug}</div>
