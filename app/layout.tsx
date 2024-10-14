751f6eead28141895ecbaa6bcbb9564147eefc25
ssabii <fhy1930@gmail.com>
2024-10-14 23:38:27 +0900

    feat: route groups 예제 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import NavLinks from "@/components/NavLinks";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
       </body>
     </html >
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="border-b p-4 text-2xl font-bold">
+      header
+    </header>
+  );
+}
+
+export default Header;
