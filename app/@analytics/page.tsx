c460a7b98e99529f4c158a89e8e154219bfd95cb
Joseph 요셉 <joseph@kmong.com>
2024-10-15 10:18:41 +0900

    feat: default 예제 구현

--- app/@analytics/page.tsx
+++ app/@analytics/page.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
-    <div className="flex h-96 items-center justify-center rounded-xl bg-purple-400 text-2xl font-bold">
-      analytics
-      </div>
+    <div className={`
+      flex h-96 items-center justify-center rounded-xl bg-purple-400 text-2xl font-bold
+    `}
+    >
+      Analytics
+    </div>
   );
 }
--- app/default.tsx
+++ app/default.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <h1 className="p-4 text-2xl font-bold">
-      Home page
+      Home Default
     </h1>
-  )
+  );
 }
