021df69985ab1c1ed8f74372f2f7c4f92feac7a3
ssabii <fhy1930@gmail.com>
2024-10-16 08:46:36 +0900

    chore: 불필요 파일 제거

--- app/parallel-routes/@users/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function UsersSettingsPage() {
-  return (
-    <h2 className='rounded-lg bg-emerald-300 p-4 text-2xl text-gray-900 shadow-md'>
-      Users Settings
-    </h2>
-  );
-}
--- app/sequential-rendering/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="p-8">
-      {children}
-    </section>
-  );
-}
