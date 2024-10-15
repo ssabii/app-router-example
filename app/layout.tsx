ca46e64dc1694a8ddd35a6b4ab55b659a747a3cb
Joseph 요셉 <joseph@kmong.com>
2024-10-15 10:19:52 +0900

    feat: NavLinks 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import NavLinks from "@/components/NavLinks";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavLinks />
         {children}
         <div className="grid grid-cols-2 gap-4 p-4">
           {team}
